bcd8fd17738c251ed03c87f2f62bfe11904ddf10
MGBChrome <roman.inglin@gmail.com>
2022-12-29 19:32:42 +0100

    Hard code credentials in ImportData.ps1

--- ImportData.ps1
+++ ImportData.ps1
@@ -3,11 +3,14 @@ $Database = 'Corona'
 $File = 'file.csv'
 
 
-Invoke-WebRequest https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv -OutFile $File
+# Invoke-WebRequest https://raw.githubusercontent.com/openZH/covid_19/master/COVID19_Fallzahlen_CH_total_v2.csv -OutFile $File
 
 # Import Anstossen mit dediziertem User mit Write-Rechten
 $Sql = "EXEC dbo.uspImportCoronaData @CsvPath = '" + $PWD + "\" + $File + "'"
-$Credentials = Get-Credential
+
+$Username = "ScriptingUser"
+$Password = ConvertTo-SecureString "S1cheresPassw0rt" -AsPlainText -Force
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList ($Username, $Password)
 
 #Invoke-Sqlcmd -ServerInstance $Server -Database $Database -Query $Sql | Out-Null
 Invoke-Sqlcmd -ServerInstance $Server -Database $Database -Query $Sql -Credential $Credentials -QueryTimeout 120 | Out-Null
