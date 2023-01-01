6c7a523b3bb91dd5550d358458136cc0d942a681
fullyonline <no.reply4me@gmail.com>
2023-01-01 13:07:59 +0100

    refactor: isnull on the view for easier handling

--- 22_USV_GetSwissData.sql
+++ 22_USV_GetSwissData.sql
@@ -15,14 +15,14 @@ CREATE VIEW usvGetSwissData
 AS
 
 SELECT 
-	SUM(CoronaDaten.Positiv) as Positiv, SUM(CoronaDaten.Verstorben) as Verstorben,
-	SUM(CoronaDaten.Isoliert) as Isoliert , SUM(CoronaDaten.InQuarantaene) as InQuarantaene,
+	ISNULL(SUM(CoronaDaten.Positiv), 0) as Positiv, ISNULL(SUM(CoronaDaten.Verstorben), 0) as Verstorben,
+	ISNULL(SUM(CoronaDaten.Isoliert), 0) as Isoliert , ISNULL(SUM(CoronaDaten.InQuarantaene), 0) as InQuarantaene,
 	Datum.Datum, Datum.DatumID,
-	(SELECT Attribut.Wert FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'current_icu' AND Attribut.DatumID = Datum.DatumID) as AktuellAufIntensivstation,
-	(SELECT Attribut.Wert FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'new_hosp' AND Attribut.DatumID = Datum.DatumID) as NeuHospitalisiert,
-	(SELECT Attribut.Wert FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'current_hosp' AND Attribut.DatumID = Datum.DatumID) as AktuellHospitalisiert,
-	(SELECT Attribut.Wert FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'ncumul_tested' AND Attribut.DatumID = Datum.DatumID) as Getestet,
-	(SELECT Attribut.Wert FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'current_vent' AND Attribut.DatumID = Datum.DatumID) as AktuellMitBeatmungsgeraet
+	(SELECT ISNULL(Attribut.Wert, 0) FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'current_icu' AND Attribut.DatumID = Datum.DatumID) as AktuellAufIntensivstation,
+	(SELECT ISNULL(Attribut.Wert, 0) FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'new_hosp' AND Attribut.DatumID = Datum.DatumID) as NeuHospitalisiert,
+	(SELECT ISNULL(Attribut.Wert, 0) FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'current_hosp' AND Attribut.DatumID = Datum.DatumID) as AktuellHospitalisiert,
+	(SELECT ISNULL(Attribut.Wert, 0) FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'ncumul_tested' AND Attribut.DatumID = Datum.DatumID) as Getestet,
+	(SELECT ISNULL(Attribut.Wert, 0) FROM Attribut JOIN AttributTyp ON Attribut.AttributTypID = AttributTyp.AttributTypID WHERE AttributTyp.AttributTyp = 'current_vent' AND Attribut.DatumID = Datum.DatumID) as AktuellMitBeatmungsgeraet
 FROM CoronaDaten
 JOIN Datum ON CoronaDaten.DatumID = Datum.DatumID
 GROUP BY Datum.Datum, Datum.DatumID
